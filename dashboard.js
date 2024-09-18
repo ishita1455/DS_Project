11527798a6cb8e203dd1f49fdbb1311d551fe019
Gunjan Pandya <145027556+G-gunjan@users.noreply.github.com>
2024-09-18 13:35:12 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("sidebar").classList.add("open");
+    document.getElementById("sidebar").classList.remove("closed");
+}
+
+function closeNav() {
+    document.getElementById("sidebar").classList.remove("open");
+    document.getElementById("sidebar").classList.add("closed");
+}
+
